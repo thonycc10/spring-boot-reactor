1e3b94684d08b780aa2f0bbd33eb1f0e22388a83
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-05-27 12:21:42 -0500

    usando lambda y creando Flux reactor

--- src/main/java/com/ac/springbootreactor/SpringBootReactorApplication.java
+++ src/main/java/com/ac/springbootreactor/SpringBootReactorApplication.java
@@ -1,13 +1,27 @@
 package com.ac.springbootreactor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+		/*Flux<String> nombres = Flux.just("Andres", "Pedro", "Juan", "Diego").doOnNext(nombre -> {
+			System.out.println(nombre);
+		});*/
+
+		// explesion lambda
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "Juan", "Diego").doOnNext(System.out::println);
+
+		nombres.subscribe();
+	}
 }
