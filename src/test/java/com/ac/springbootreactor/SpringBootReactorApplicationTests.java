c9db96682fd5fa0fb47bdc52474eabf3235d0caf
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-05-27 11:49:20 -0500

    first commit - start app console reactor

---
+++ src/test/java/com/ac/springbootreactor/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.springbootreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
